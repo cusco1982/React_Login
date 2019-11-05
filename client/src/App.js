e0be9b1cfaf1de54af2860a09411b5eb3139da58
Julian <cusco2891@gmail.com>
2019-11-05 13:14:55 -0500

    imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import Profile from './components/Profile';
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
+import Signup from './components/Signup';
+
+
+
+
 export default class App extends Component {
 
-    render() {
+
+  render() {
 
 
-      // JSX
-        return(
-            <div>
-                <h2>I am the App Component</h2>
-            </div>
-        )
-    }
+    // JSX
+    return (
+      <Router>
+
+
+        <Route path="/" component={Header} />
+
+        <div>
+          <h2>I am the App Component</h2>
+        </div>
+
+
+      </Router>
+    );
+  }
 }
