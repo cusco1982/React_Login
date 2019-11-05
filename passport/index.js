4d5cdb9173e4c964b8ef7276e13b1803eecd4196
Julian <cusco2891@gmail.com>
2019-11-05 14:05:04 -0500

    indexjs passport

---
+++ passport/index.js
@@ -0,0 +1,10 @@
+const passport = require ('passport');
+
+// import all the strategies
+const SignupStrategy = require("./GoogleStrategy");
+const SigninStrategy = require("./GoogleStrategy");
+// const GoogleStrategy = require("./GoogleStrategy");
+
+
+passport.use('local-signin', SigninStrategy);
+passport.use('local-signup', SignupStrategy);
