810c4b3d3981a87f39c8740da5b3a9f3041b789a
Julian <cusco2891@gmail.com>
2019-11-05 12:41:11 -0500

    profile component

---
+++ client/src/components/Profile.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Profile extends Component {
+
+    render() {
+
+        return(
+            <div>
+                <h2>I am the Profile Component</h2>
+            </div>
+        )
+    }
+}
