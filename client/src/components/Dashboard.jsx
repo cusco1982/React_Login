b76db4514eddc3f8167c5dc52fa33f0422a6661c
Julian <cusco2891@gmail.com>
2019-11-05 12:38:19 -0500

    dashboard component

---
+++ client/src/components/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Dashboard extends Component {
+
+    render() {
+
+        return(
+            <div>
+                <h2>I am the Dashboard Component</h2>
+            </div>
+        )
+    }
+}
