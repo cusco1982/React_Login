46086fbf517a44e5d5c653ab46c811e1bb0bf492
Julian <cusco2891@gmail.com>
2019-11-05 12:35:09 -0500

    signup component

---
+++ client/src/components/Signup.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Signup extends Component {
+
+    render() {
+
+        return(
+            <div>
+                <h2>I am the Signup Component</h2>
+            </div>
+        )
+    }
+}
