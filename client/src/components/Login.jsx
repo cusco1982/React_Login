2001157dae96dc2cd6723a0de06d215274ecd78b
Julian <cusco2891@gmail.com>
2019-11-06 22:18:52 -0500

    signin template

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,13 +1,46 @@
 import React, { Component } from 'react';
+import axios from "axios"
 
-export default class Login extends Component {
+
+
+export default class Signup extends Component {
+    state = {
+        email: "",
+        password: ""
+    };
+
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+
+        const { email, password } = this.state;
+        axios.post({
+            url: "/authentication/signup",
+            method: 'POST',
+            data: {
+                email,
+                password
+            }
+        })
+    }
+    handleChange = (event) => {
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value
+        });
+    };
+
 
     render() {
-
-        return(
+        return (
             <div>
-                <h2>I am the Login Component</h2>
+                <form>
+                    <input type="text" name="email" onChange={this.handleChange} />
+                    <input type="password" name="password" onChange={this.handleChange} />
+                    <button>Signup</button>
+                </form>
             </div>
         )
     }
+
 }
